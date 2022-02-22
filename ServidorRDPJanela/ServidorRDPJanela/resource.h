46b1de35d90ce300102ea21113b3a5c44f5e7d90
lcmarincek <52965503+lcmarincek@users.noreply.github.com>
2022-02-22 17:49:33 -0300

    Add files via upload

---
+++ ServidorRDPJanela/ServidorRDPJanela/resource.h
@@ -0,0 +1,26 @@
+//{{NO_DEPENDENCIES}}
+// Arquivo de inclusão gerado pelo Microsoft Visual C++.
+// Usado por WindowsProject1.rc
+//
+#define IDC_MYICON                      2
+#define IDD_WINDOWSPROJECT1_DIALOG      102
+#define IDS_APP_TITLE                   103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_WINDOWSPROJECT1             107
+#define IDI_SMALL                       108
+#define IDC_WINDOWSPROJECT1             109
+#define IDR_MAINFRAME                   128
+#define IDC_LISTA_JANELA                1001
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        131
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1002
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
